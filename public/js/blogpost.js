0796d16f829b6d040391db03d6e3d14d8fc15467
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-01-14 15:15:18 -0800

    added view and comment buttons for blogposts

--- public/js/blogpost.js
+++ public/js/blogpost.js
@@ -1,4 +1,10 @@
-$('.readMore').on('click', function (e) {
+$('.viewPost').on('click', function (e) {
     e.preventDefault()
     window.location.replace('/blogpost/' + $(this).attr('name'))
+})
+
+$('#commentPost').on('click', function (e) {
+    console.log('comment post button clicked')
+    // e.preventDefault()
+    // window.location.replace('/blogpost/' + $(this).attr('name'))
 })
